59355add2ff7391b13d3858f56f6026c8b5c922c
victrosantos <victor.santos.fk@hotmail.com>
2023-07-13 10:05:45 -0300

    fix: Corrigido matrixError

--- src/middleware/matrixError.py
+++ src/middleware/matrixError.py
@@ -1,6 +1,4 @@
 def matrixError(matrix, length):
-    num_cols = len(matrix[0]) if length > 0 else 0
-
-    if length != num_cols:
+    if any(len(row) != length for row in matrix):
         raise ValueError(
             'Invalid matrix dimensions. The matrix must be square (NxN).')
